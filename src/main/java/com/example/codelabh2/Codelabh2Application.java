95e9e59ba1dd89e4e11978dd1e35cedf6e90b13a
santiarojas18 <123812766+santiarojas18@users.noreply.github.com>
2024-03-01 16:32:10 -0500

    Add files via upload

---
+++ src/main/java/com/example/codelabh2/Codelabh2Application.java
@@ -0,0 +1,13 @@
+package com.example.codelabh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codelabh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codelabh2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codelabh2/Codelabh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codelabh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codelabh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
